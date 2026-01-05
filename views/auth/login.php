889ef2debd5f9a2de9c3d7878b2c5a4857ad06ac
aziz <azizmarhiansyah@gmail.com>
2026-01-05 13:54:57 +0700

    login aziz

--- views/auth/login.php
+++ views/auth/login.php
@@ -8,6 +8,7 @@ session_start();
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login / Register - VANGUARD</title>
+    <link rel="icon" type="image/png" href="../../assets/img/Home/L_Vg.png">
 
     <!-- TEMPLATE CSS -->
     <link rel="stylesheet" href="../../assets/css/login.css">
